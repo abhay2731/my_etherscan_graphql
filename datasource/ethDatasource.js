1647761c4dc81a8138d651a6d3d7afe2e7deb69e
abhay2731 <113664848+abhay2731@users.noreply.github.com>
2023-11-21 14:42:03 +0530

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,41 @@
+const { RESTDataSource } = require("apollo-datasource-rest");// Import the RESTDataSource class from Apollo to create a REST data source
+
+//Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+//Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    // Set the base URL for Etherscan's API
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+  // Get ETH balance for a specific address
+  async etherBalanceByAddress() {
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+   // Get total supply of ETH
+  async totalSupplyOfEther() {
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  //Paste Code Here For New API Endpoints
+  // Get latest ETH price
+  async getLatestEthereumPrice() {
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+  // Get estimated block confirmation time
+  async getBlockConfirmationTime() {
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+}
+
+module.exports = EtherDataSource;
